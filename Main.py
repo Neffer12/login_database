c7d9c1333eb5709ce34174b2af6ccfb16568932a
Neffer12 <65372614+Neffer12@users.noreply.github.com>
2021-10-26 17:58:40 -0500

    Add files via upload
    Initial Commit- Program designed to create a login database using tkinter.

---
+++ Main.py
@@ -0,0 +1,38 @@
+import sqlite3
+from tkinter import *
+
+root = Tk()
+root.geometry("400x400")
+
+# def submit():
+
+
+
+# connect to database
+conn = sqlite3.connect('users.db')
+
+# create a cursor
+c = conn.cursor()
+
+# create a table
+c.execute("""CREATE TABLE IF NOT EXISTS users(
+    user_ID integer primary key autoincrement not null,
+    username text not null,
+    password text not null
+)""")
+
+# user input
+for i in range(1):
+    username = input("Username: ").capitalize()
+    password = input("Password: ").capitalize()
+
+# inserting data into table
+conn.execute("INSERT INTO characters VALUES ('{}','{}')".format(username,password))
+
+# commit command
+conn.commit()
+
+# close connection
+conn.close()
+
+root.mainloop()
